e59b40b12440c6ba12245ad82cd1f3e0d4a92f47
Evan Jacobs <emjacobs494@gmail.com>
2018-02-12 23:27:00 -0800

    Added name

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+const name = 'test';
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+
+        <Text>My name is {name}</Text>
+        <Text>Evam Jacobs</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
