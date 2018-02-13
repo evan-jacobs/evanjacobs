1109c51ab966d659be2fd0230eccdfbad47e51ca
Evan Jacobs <emjacobs494@gmail.com>
2018-02-13 00:46:07 -0800

    Fixed typo on name

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
 
         <Text>My name is {name}</Text>
-        <Text>Evam Jacobs</Text>
+        <Text>Evan Jacobs</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
